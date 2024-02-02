f6d6770a1b8f97d146eeaaba58074da2ac2a3166
Saikumar guduru <87139133+skg1312@users.noreply.github.com>
2024-02-02 23:14:30 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -6,80 +6,82 @@ const fs = require('fs');
 const path = require('path');
 
 router.get('/:id', async (req, res) => {
-	var invoiceId = req.params.id;
-	try {
-		const result = await Invoice.findById(invoiceId);
+    var invoiceId = req.params.id;
 
-		// Throw error if no result
-		if (!result) {
-			throw { name: 'Error' };
-		}
+    try {
+        const result = await Invoice.findById(invoiceId);
 
-		// var companyid = result.companydetails.companyid;
-		// Continue process if result is returned
-		await custom.generatePdf(
-			{ filename: invoiceId, format: 'A4' },
-			result,
-			async (fileLocation) => {
-				return res.download(fileLocation, (error) => {
-					if (error)
-						res.status(500).json({
-							success: false,
-							result: null,
-							message: "Couldn't find file",
-							error: error.message,
-						});
-				});
-			}
-		);
-	} catch (error) {
-		// If error is thrown from DB
-		if (error.name == 'Error') {
-			return res.status(400).json({
-				success: false,
-				result: null,
-				error: error.message,
-				message: 'Data not available',
-			});
-		} else if (error.name == 'BSONTypeError') {
-			return res.status(400).json({
-				success: false,
-				result: null,
-				error: error.message,
-				message: 'Invalid ID',
-			});
-		} else {
-			// Server Error
-			console.log(error);
-			return res.status(500).json({
-				success: false,
-				result: null,
-				error: error.message,
-				message: error.message,
-			});
-		}
-	}
+        // Throw error if no result
+        if (!result) {
+            throw { name: 'Error' };
+        }
+
+        // Continue process if result is returned
+        await custom.generatePdf(
+            { filename: invoiceId.toString(), format: 'A4' },
+            result,
+            async (fileLocation) => {
+                console.log('File location:', fileLocation);
+
+                return res.download(fileLocation, (error) => {
+                    if (error) {
+                        res.status(500).json({
+                            success: false,
+                            result: null,
+                            message: "Couldn't find file",
+                            error: error.message,
+                        });
+                    }
+                });
+            }
+        );
+    } catch (error) {
+        // Handle errors
+        if (error.name == 'Error') {
+            return res.status(400).json({
+                success: false,
+                result: null,
+                error: error.message,
+                message: 'Data not available',
+            });
+        } else if (error.name == 'BSONTypeError') {
+            return res.status(400).json({
+                success: false,
+                result: null,
+                error: error.message,
+                message: 'Invalid ID',
+            });
+        } else {
+            // Server Error
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                result: null,
+                error: error.message,
+                message: error.message,
+            });
+        }
+    }
 });
+
 router.get('/pdf/:id', async (req, res) => {
-	const invoiceId = req.params.id;
-	const fileLocation = path.join(
-		__dirname,
-		`../public/download/${invoiceId}.pdf`
-	);
+    const invoiceId = req.params.id;
+    const fileLocation = path.join(__dirname, `../public/download/${invoiceId}.pdf`);
 
-	// Check if the file exists
-	if (fs.existsSync(fileLocation)) {
-		// Read the PDF file and send it as a response
-		const fileStream = fs.createReadStream(fileLocation);
-		fileStream.pipe(res);
-	} else {
-		// If the file does not exist, send a 404 response
-		res.status(404).json({
-			success: false,
-			result: null,
-			message: "Couldn't find file",
-			error: 'File not found',
-		});
-	}
+    // Check if the file exists
+    if (fs.existsSync(fileLocation)) {
+        // Read the PDF file and send it as a response
+        const fileStream = fs.createReadStream(fileLocation);
+        fileStream.pipe(res);
+    } else {
+        // If the file does not exist, send a 404 response
+        res.status(404).json({
+            success: false,
+            result: null,
+            message: "Couldn't find file",
+            error: 'File not found',
+        });
+    }
 });
+
 module.exports = router;
